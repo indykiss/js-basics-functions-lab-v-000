1a7cbcbe39e6e7a717a0c1d57978273a79b5ad62
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-27 22:07:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function distanceFromHqInBlocks(someValue){
 }
 
 function distanceFromHqInFeet (someValue) {
-  distanceFromHqInBlocks(someValue) * 264;
+  return distanceFromHqInBlocks(someValue) * 264;
   
 }
 
