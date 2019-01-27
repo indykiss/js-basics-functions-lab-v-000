ef7750b81ef8042000fc5ff7389be9a8186acb11
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-27 22:10:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ function distanceTravelledInFeet(startBlock, endBlock) {
   if (startBlock > endBlock) {
     return (startBlock - endBlock) * 264;}
       else (endBlock - startBlock) {
-      return (endBlock - startBlock) * 264;
-      }
+      return (endBlock - startBlock) * 264;}
 }
 
 
